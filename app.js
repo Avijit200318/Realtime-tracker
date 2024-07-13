ceee7c5120f53fbbc68a9f8d1962b40e10be4722
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2024-07-13 12:51:03 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+const http = require("http");
+const socketio = require("socket.io");
+
+const server = http.createServer(app);
+
+const io = socketio(server);
+
+app.get("/", (req, res) => {
+    res.send("hello world");
+})
+
+server.listen(3000, () => {
+    console.log("server is running at port 3000");
+});
