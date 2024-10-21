5b1d2b09a056b57fb63402a92301cd735558bf73
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2024-10-21 13:40:41 +0530

    added custom maerkers

--- react map integration/client/src/Pages/MapTwo.jsx
+++ react map integration/client/src/Pages/MapTwo.jsx
@@ -2,8 +2,20 @@ import React from 'react';
 import {MapContainer, TileLayer, CircleMarker, Popup, Marker} from "react-leaflet";
 import 'leaflet/dist/leaflet.css';
 
+import L from 'leaflet';  // Import leaflet to use L.icon
+import markerIcon from '/images/marker.png';  // Import the marker image
+
 
 export default function MapTwo() {
+
+    // create the icon L.Icon and image
+    const maerkerIcon = new L.Icon({
+        iconUrl: markerIcon,
+        iconSize: [35, 45],
+        iconAnchor: [17, 45],  // set the marker position properly (x, y)
+        popupAnchor: [0, -45],  // Point where the popup will appear relative to the icon
+    });
+
   return (
     <MapContainer center={[22.5744, 88.3629]} zoom={14} scrollWheelZoom={false}>
         <TileLayer attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>' url='https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=xpL7wNLWmyhEZZoEtLXq'/>
@@ -21,7 +33,7 @@ export default function MapTwo() {
         <Marker position={[22.595770, 88.263641]}>
             <Popup>Howrah</Popup>
         </Marker>
-        <Marker position={[22.5678, 88.3710]}>
+        <Marker position={[22.5678, 88.3710]} icon={maerkerIcon}>
             <Popup>Sealdha</Popup>
         </Marker>
 
